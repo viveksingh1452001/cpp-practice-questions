5e50f33cabaf6ce3fe15c1055a98648cabbb5308
viveksingh1452001 <vs2467@srmist.edu.in>
2021-09-05 18:21:29 +0530

    add

--- tut12.cpp
+++ tut12.cpp
@@ -131,7 +131,7 @@ int main(){
 //******************************************************
 /*
 #include <iostream>
-using namespace std;
+ using namespace std;
 
 int main()
 {
@@ -385,3 +385,290 @@ int main(){
 //     }
 //     return 0;
 // }
+
+
+
+//*************************************
+//-------------butterfly---------------
+//*************************************
+/*
+                 *             *
+                 * *         * *
+                 * * *     * * *   
+                 * * * * * * * *
+                 * * * * * * * *
+                 * * *     * * *
+                 * *         * *
+                 *             *
+                                     */
+  /*
+    #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            int space=2*n-2*i;
+            for(int j=1; j<=space; j++){
+                cout<<" ";
+            }
+             for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            int space=2*n-2*i;
+            for(int j=1; j<=space; j++){
+                cout<<" ";
+            }
+             for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
+    */
+
+
+
+   //**********************************
+   //------inverted pattern------------
+   //**********************************
+   /*
+               1 2 3 4 5
+               1 2 3 4
+               1 2 3
+               1 2
+               1
+                         */
+  /*
+    #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n+1-i; j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
+    */
+
+
+
+
+   //*************************************
+   //--------0-1 pattern triangle---------
+   //*************************************
+   /*
+                  1
+                  0 1
+                  1 0 1
+                  0 1 0 1
+                  1 0 1 0 1
+                             */
+ /*   #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    cout<<"1 ";
+                }
+                else{
+                    cout<<"0 ";
+                }
+            }
+            cout<<endl;
+        }
+        return 0;
+    } */
+
+
+
+
+    //***************************
+    //------rhombus-------------
+    //***************************
+    /*
+              * * * * * *
+             * * * * * *
+            * * * * * *
+           * * * * * *
+          * * * * * *
+                      */
+ /*   #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                cout<<"  ";
+            }
+            for(int j=1; j<=n; j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }  */
+
+
+
+//***************************
+//------no. pyramid---------
+//***************************
+/*
+             1
+            1 2
+           1 2 3
+          1 2 3 4
+         1 2 3 4 5
+                   */
+                  
+/*    #include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  */
+
+
+
+
+//*******************************
+//--------palindrom pyramid------
+//*******************************
+/*
+                 1
+               2 1 2
+             3 2 1 2 3
+           4 3 2 1 2 3 4
+         5 4 3 2 1 2 3 4 5
+                           */
+ /*   #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            int j;
+            for( j=1; j<=n-i; j++){
+                cout<<"  ";
+            }
+            int k=i;
+            for(;j<=n;j++){
+                cout<<k--<<" ";
+            }
+            k=2;
+            for(;j<=n+i-1;j++){
+                cout<<k++<<" ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
+    */
+
+
+
+   //********************************************************
+   /*
+                      *
+                    * * * 
+                  * * * * * 
+                * * * * * * *
+                * * * * * * *
+                  * * * * * 
+                    * * *
+                      *
+                             */
+ /*   #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                cout<<"  ";
+            }
+            int star=2*i-1;
+            for(int j=1; j<=star; j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
+                cout<<"  ";
+            }
+            int star=2*i-1;
+            for(int j=1; j<=star; j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        return 0;
+    }                        */
+
+
+
+    //*****************************
+    //--------zig-zag -------------
+    //*****************************
+    /*
+             *         *
+           *   *     *   *
+         *       *  *      *
+       *          *          *
+                                      */
+  /*  #include<iostream>
+    using namespace std;
+    
+    int main(){
+        int n;
+        cin>>n;
+        for(int i=1; i<=3;i++){
+            for(int j=1; j<=n; j++){
+                if((i+j)%4==0 || (i==2 && j%4==0)){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            cout<<endl;
+        }
+        return 0;
+    }            */                     
